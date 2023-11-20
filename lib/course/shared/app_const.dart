8dbc368ff786370dcdde5155efe257cd82a38874
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-11-20 10:26:16 +0530

    appConst and colourConst class implement

---
+++ lib/course/shared/app_const.dart
@@ -0,0 +1,8 @@
+class AppConst {
+  static const String course = 'Course';
+  static const String courseName = 'Course Name';
+  static const String imgUrl = 'imgUrl';
+  static const String add = 'Add';
+  static const String updateCourse = 'Update Course';
+  static const String update = 'Update ';
+}
