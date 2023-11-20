ca91577c22ac2466a7b017912a11a3a2ed15e6cf
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-11-20 10:27:06 +0530

    main implement

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:codexia_learning_admin/firebase_options.dart';
+import 'package:codexia_learning_admin/login/screen/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AuthScreen(),
+      // home: ShowCourseScreen(courseService: CourseService(),),
+    );
+  }
+}
