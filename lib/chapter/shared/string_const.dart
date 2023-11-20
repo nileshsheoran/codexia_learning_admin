91afdcc8ac552807f1feef9f22f14a24f4254cd8
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-11-20 10:09:13 +0530

    stringConst class create

---
+++ lib/chapter/shared/string_const.dart
@@ -0,0 +1,9 @@
+class StringConst {
+  static const String addChapter = 'Add Chapter';
+  static const String add = 'Add';
+  static const String chapters = 'Chapters';
+  static const String updateChapter = 'Update Chapter';
+  static const String update = 'Update ';
+  static const String chapterName = 'Chapter Name';
+  static const String chapterContent = 'Chapter Content';
+}
